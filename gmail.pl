dbfa11a98d39dd7509882a1b91ad1aa78c5e97d8
Matti Hiljanen <matti@hiljanen.com>
2009-05-11 08:28:33 +0000

    fixes and cleanups

--- gmail.pl
+++ gmail.pl
@@ -19,22 +19,23 @@ my $forked;
  
 sub count {
     my $api=XML::Atom::Client->new;
-    my ($l,$p) = (Irssi::settings_get_str('gmail_user'), Irssi::settings_get_str('gmail_pass'));
-    return 0 if ($l eq "" || $p eq "");
+    my ($l,$p,$f) = (Irssi::settings_get_str('gmail_user'), Irssi::settings_get_str('gmail_pass'), 
+            Irssi::settings_get_str('gmail_feed'));
+    return -2 if ($l eq "" || $p eq "");
  
     $api->username($l);
     $api->password($p);
  
-    my $feed=$api->getFeed("https://mail.google.com/mail/feed/atom");
+    my $feed=$api->getFeed($f);
  
     if($feed) {
         if($feed->as_xml =~ /<fullcount>(.*)<\/fullcount>/g) {
             return int($1);
         } else {
-            return 0;
+            return -1;
         }
     } else {
-      return 0;
+      return -1;
     }
 }
  
@@ -62,10 +63,27 @@ sub update {
         my $target = {fh => $$rh, tag => undef};
         $target->{tag} = Irssi::input_add(fileno($rh), INPUT_READ, \&read_pipe, $target);
     } else {
+        my $ret = -3;
         eval {
             local $SIG{'__WARN__'} = sub { die "@_" };
-            print $wh count() . "\n";
+            $ret = count();
         };
+        if ($@) {
+            my $err = $@;
+            chomp $err;
+            $err =~ s/\n/-/g;
+            print $wh "0\nERR $err\n";
+        } else {
+            if ($ret >= 0) {
+                print $wh $ret . "\nOK\n";
+            } elsif ($ret == -1) {
+                print $wh "0\nERR getting feed failed\n";
+            } elsif ($ret == -2) {
+                print $wh "0\nERR l/p not set, update skipped\n";
+            } else {
+                print $wh "0\nERR unknown error\n";
+            }
+        }
         close $rh;
         close $wh;
         POSIX::_exit(1);
@@ -86,7 +104,13 @@ sub read_pipe {
     Irssi::input_remove($target->{tag});
     $forked = 0;
 
+    my $status = pop @rows;
+
     $count = $rows[0];
+    
+    if (Irssi::settings_get_bool('gmail_debug')) { 
+        Irssi::print("updated, status is $status, count is $count");
+    }
 
     refresh();
 }
@@ -108,6 +132,12 @@ sub mail {
 Irssi::statusbar_item_register("mail", undef, "mail");
 Irssi::settings_add_str('gmail', 'gmail_user', '');
 Irssi::settings_add_str('gmail', 'gmail_pass', '');
+Irssi::settings_add_str('gmail', 'gmail_feed', 'https://mail.google.com/mail/feed/atom');
+Irssi::settings_add_bool('gmail', 'gmail_debug', 0);
+
+Irssi::print("GMail.pl loaded. Remember to set your username and password (gmail_user and gmail_pass) " 
+        . "and add \"mail\" statusbar item to your statusbar. "
+        . "You can enable debugging with /toggle gmail_debug");
  
 update();
 Irssi::timeout_add(60*1000, "update", undef);
